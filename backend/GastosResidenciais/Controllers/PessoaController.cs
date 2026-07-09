e496f7400011c792270abeb306360b2973b13f25
Marcos Henrique <marcosfhtrabalho@gmail.com>
2026-07-08 22:59:22 -0300

    feat: adiciona um controller para as funcionalidades da entidade Pessoa

---
+++ backend/GastosResidenciais/Controllers/PessoaController.cs
@@ -0,0 +1,25 @@
+﻿using GastosResidenciais.Dtos.Pessoa;
+using GastosResidenciais.Models;
+using GastosResidenciais.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GastosResidenciais.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PessoaController : ControllerBase
+{
+    private readonly IPessoaService service;
+
+    public PessoaController(IPessoaService service)
+    {
+        this.service = service;
+    }
+
+    [HttpPost]
+    public IActionResult CadastrarPessoa([FromBody] CriarPessoaRequest request)
+    {
+        var response = service.Cadastrar(request);
+        return StatusCode(StatusCodes.Status201Created, response);
+    }
+}
