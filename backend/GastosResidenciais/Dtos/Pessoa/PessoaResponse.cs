b2d1364d42598d8fbe6f82460d418e89caa9b353
Marcos Henrique <marcosfhtrabalho@gmail.com>
2026-07-08 22:01:56 -0300

    feat: cria os dtos de responde da entidade Pessoa e Transacao

---
+++ backend/GastosResidenciais/Dtos/Pessoa/PessoaResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace GastosResidenciais.Dtos.Pessoa;
+
+public record PessoaResponse(Guid Id, string Nome, int Idade);
---
+++ backend/GastosResidenciais/Dtos/Transacao/TransacaoResponse.cs
@@ -0,0 +1,11 @@
+﻿using GastosResidenciais.Enums;
+
+namespace GastosResidenciais.Dtos.Transacao;
+
+public record TransacaoResponse(
+    Guid Id,
+    string Descricao,
+    decimal Valor, 
+    TransacaoTipo Tipo,
+    Guid PessoaId
+);
