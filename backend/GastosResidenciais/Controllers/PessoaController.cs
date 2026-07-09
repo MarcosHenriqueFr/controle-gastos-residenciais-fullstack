dc46a99a2918bf707e3e783fffad213db87caab4
Marcos Henrique <marcosfhtrabalho@gmail.com>
2026-07-09 20:31:56 -0300

    fix: adapta o controller de pessoa para o processamento async do banco de dados

--- backend/GastosResidenciais/Controllers/PessoaController.cs
+++ backend/GastosResidenciais/Controllers/PessoaController.cs
@@ -1,25 +1,57 @@
 ﻿using GastosResidenciais.Dtos.Pessoa;
-using GastosResidenciais.Models;
 using GastosResidenciais.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GastosResidenciais.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/pessoa")]
 public class PessoaController : ControllerBase
 {
-    private readonly IPessoaService _service;
+    private readonly IPessoaService _pessoaService;
 
-    public PessoaController(IPessoaService service)
+    public PessoaController(IPessoaService pessoaService)
     {
-        _service = service;
+        _pessoaService = pessoaService;
     }
 
     [HttpPost]
-    public IActionResult CadastrarPessoa([FromBody] CriarPessoaRequest request)
+    public async Task<IActionResult> Criar([FromBody] CriarPessoaRequest request)
     {
-        var response = _service.Cadastrar(request);
-        return StatusCode(StatusCodes.Status201Created, response);
+        var pessoa = await _pessoaService.CriarAsync(request);
+
+        return CreatedAtAction(nameof(ObterPorId), new { id = pessoa.Id }, pessoa);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ObterTodas()
+    {
+        var pessoas = await _pessoaService.ObterTodasAsync();
+
+        return Ok(pessoas);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> ObterPorId(Guid id)
+    {
+        var pessoa = await _pessoaService.ObterPorIdAsync(id);
+
+        return Ok(pessoa);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<IActionResult> Remover(Guid id)
+    {
+        await _pessoaService.RemoverAsync(id);
+
+        return NoContent();
+    }
+
+    [HttpGet("{id:guid}/transacaos")]
+    public async Task<IActionResult> ObterTransacoesPorPessoa(Guid id)
+    {
+        var transacoes = await _pessoaService.ObterTransacoesPorPessoaAsync(id);
+
+        return Ok(transacoes);
     }
 }
