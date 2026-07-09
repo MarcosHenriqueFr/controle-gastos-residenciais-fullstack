3a8a2e022565bd02717e81ee1510ed437851d0e6
Marcos Henrique <marcosfhtrabalho@gmail.com>
2026-07-09 20:08:53 -0300

    feat: Cria mappers para fazer a conversão entre entidades e dtos no sistema

---
+++ backend/GastosResidenciais/Mappers/PessoaMapper.cs
@@ -0,0 +1,17 @@
+﻿using GastosResidenciais.Dtos.Pessoa;
+using GastosResidenciais.Models;
+
+namespace GastosResidenciais.Mappers;
+
+public static class PessoaMapper
+{
+    public static Pessoa ToEntity(CriarPessoaRequest request)
+    {
+        return new Pessoa(request.Nome, request.Idade);
+    }
+
+    public static PessoaResponse ToDto(Pessoa pessoa)
+    {
+        return new PessoaResponse(pessoa.Id, pessoa.Nome, pessoa.Idade);
+    }
+}
---
+++ backend/GastosResidenciais/Mappers/TransacaoMapper.cs
@@ -0,0 +1,17 @@
+﻿using GastosResidenciais.Dtos.Transacao;
+using GastosResidenciais.Models;
+
+namespace GastosResidenciais.Mappers;
+
+public static class TransacaoMapper
+{
+    public static Transacao ToEntity(CriarTransacaoRequest request)
+    {
+        return new Transacao(request.Descricao, request.Valor, request.Tipo, request.PessoaId);
+    }
+
+    public static TransacaoResponse ToDto(Transacao transacao)
+    {
+        return new TransacaoResponse(transacao.Id, transacao.Descricao, transacao.Valor, transacao.Tipo, transacao.PessoaId);
+    }
+}
