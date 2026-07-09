af264205929b5b3e81ec6b067751ff401c90c251
Marcos Henrique <marcosfhtrabalho@gmail.com>
2026-07-08 21:40:10 -0300

    feat: configura como vão ficar as tabelas no banco de dados.

---
+++ backend/GastosResidenciais/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GastosResidenciais.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GastosResidenciais.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Pessoa> Pessoas { get; set; }
+    public DbSet<Transacao> Transacoes { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+    // Essa configuração permite que toda entidade de transação seja excluída conforme a ação da entidade pai
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Transacao>()
+            .HasOne(t => t.Pessoa)
+            .WithMany(p => p.Transacoes)
+            .HasForeignKey(t => t.PessoaId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
